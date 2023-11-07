3472c17342b7d2a6c84247800d44cb982568b436
thanhtikezaw <thanhtikezaw919@gmail.com>
2023-11-07 21:11:45 +0630

    added image uploading feature

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,20 +10,42 @@ class PostController extends Controller
 {
     //
     public function getPosts(Post $post){
-        return response()->json(['data'=>$post->get()],200);
+        $posts = $post->get();
+
+        foreach ($posts as $post) {
+            if (strpos($post->image_name, 'https://') !== 0) {
+                // Get the file name from the original image_name
+                $imageName = basename($post->image_name);
+
+                // Set the image path to the local directory using public_path
+                $post->image_name = asset('images') . '/' . $imageName;
+            }
+        }
+
+        return response()->json(['data' => $posts], 200);
     }
 
     public function createPost(Request $request,Post $post){
-            $post->title=$request->title;
-            $post->content=$request->content;
-            $post->content_type=$request->content_type;
-            $post->image_name=$request->image_name??null;
-            $post->save();
+        $post->title=$request->title;
+        $post->content=$request->content;
+        $post->content_type=$request->content_type;
+        $post->image_name=$request->image_name??'';
+        if($request->file('image_name')){
+            $image = $request->file('image_name');
+            $imageName = time().'.'.$image->getClientOriginalExtension();
+            $image->move(public_path('images'), $imageName); // Move the uploaded file to a directory
 
-            return response()->json([
-                'message'=>'successfully created',
-                'status'=>'success'
-            ],200);
+            $post->image_name=$imageName??null;
+
+        }
+
+        $post->save();
+
+        return response()->json([
+            'message'=>'successfully created',
+            'status'=>'success'
+        ],200);
+
 
 
     }
