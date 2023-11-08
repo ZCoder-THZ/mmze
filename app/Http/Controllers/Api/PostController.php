2ce8da3d49c104bb4cd3bfaf550db693f8920c1d
thanhtikezaw <thanhtikezaw919@gmail.com>
2023-11-09 00:39:21 +0630

    update method

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
-
+use Storage;
 class PostController extends Controller
 {
     //
@@ -42,6 +42,7 @@ class PostController extends Controller
         $post->save();
 
         return response()->json([
+            'data'=>$post,
             'message'=>'successfully created',
             'status'=>'success'
         ],200);
@@ -50,11 +51,15 @@ class PostController extends Controller
 
     }
 
+
     public function deletePost(Post $post,$id){
         $post = $post::find($id);
 
         if ($post) {
+            $imageName=$post->image_name;
+            Storage::delete(['file', 'otherFile']);
             $post->delete();
+
             return response()->json(['message' => 'Post deleted successfully'], 200);
         } else {
             return response()->json(['message' => 'Post not found'], 404);
@@ -62,16 +67,34 @@ class PostController extends Controller
     }
 
 
-    public function updatePost(Request $request,Post $post,$id){
-        $post = $post::find($id);
+    public function updatePost(Request $request, Post $post, $id){
+        $post = $post::find($id); // No need to find the post again as $post is already passed as a parameter
 
-        if ($post) {
-            $post->update($request->all());
-            return response()->json(['message' => 'Post deleted successfully'], 200);
-        } else {
+        if($post){
+            $post->title=$request->title??$post->title;
+            $post->content=$request->content??$post->content;
+            $post->content_type=$request->content_type??$post->content_type;
+            $post->image_name=$post->image_name;
+
+            if($request->file('image_name')){
+                $image = $request->file('image_name');
+                $imageName = time().'.'.$image->getClientOriginalExtension();
+                $image->move(public_path('images'), $imageName); // Move the uploaded file to a directory
+
+                $post->image_name=$imageName??null;
+
+            }
+
+            $post->save();
+            return response()->json(['message' => 'Post updated successfully','data'=>$post], 200);
+
+        }else{
             return response()->json(['message' => 'Post not found'], 404);
+
         }
     }
+
+
 
 
 }
